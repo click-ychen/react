8ca862731c109899001f47c87cb1ffea6a936e43
Yingying Chen <dinnerparty718@gmail.com>
2024-02-06 18:04:29 -0700

    feat: init
    Signed-off-by: Yingying Chen <dinnerparty718@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const [isCounting, setIsCounting] = useState(false);
+
+  const timer = useRef(null);
+
+  const onClickHandler = () => {
+    setIsCounting((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isCounting) {
+      timer.current = setInterval(() => {
+        setCount((prev) => (prev + 1) % 10);
+      }, 1000);
+    } else {
+      clearInterval(timer.current);
+    }
+
+    return () => clearInterval(timer.current);
+  }, [isCounting]);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={onClickHandler}>{isCounting ? "Stop" : "Start"}</button>
+    </div>
+  );
+}
+
+export default App;
